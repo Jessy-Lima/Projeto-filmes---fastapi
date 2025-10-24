b29becff346c22954c94b8096c2d999773e44748
Jessica Lima <maytajessica73@gmail.com>
2025-10-24 14:49:13 -0300

    CRUD completo do cinema

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -20,3 +20,72 @@ def criar_tabela():
             cursor.close()
             conexao.commit()
 
+# def cadastrar_filme(titulo, genero, ano, nota):
+#     conexao, cursor = conector()
+#     if conexao:
+#         try:
+#             cursor.execute(
+#                 "INSERT INTO filmes (titulo, genero, ano, nota) VALUES (%s,%s,%s,%s)",
+#                 (titulo, genero, ano, nota)
+#             )
+#             conexao.commit()
+#         except Exception as erro:
+#             print(f"Erro ao cadastrar o filme {erro}")
+#         finally:
+#             cursor.close()
+#             conexao.commit()
+# cadastrar_filme("Era do gelo 3", "aventura","2009","9")
+
+def listar_filmes():
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+            "SELECT * FROM filmes ORDER BY id"
+            )
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao listar os filmes {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+        
+print(listar_filmes())
+
+def atualizar_filme(id_filme, nova_nota):
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDATE filmes SET nota = %s WHERE id = %s",
+                (nova_nota, id_filme)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao atualizar nota do filme {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+print(atualizar_filme(10,2))
+
+def deletar_filme(id_filme):
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM filmes WHERE id = %s",
+                (id_filme,)
+            )
+            conexao.commit()
+            if cursor.rowcount > 0:
+                print(f"Filme removido com sucesso!")
+            else:
+                print("Nenhum filme foi encontrado com o ID fornecido.")
+        except Exception as erro:
+            print(f"Erro ao tentar deletar o filme {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+deletar_filme(3)
